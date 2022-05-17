3b4bc371760f5079a16402d17603c0127fe3d06a
Serhii Mashentsev <d.ashesss@gmail.com>
2022-05-17 23:51:38 +0300

    text message response

--- pkg/app/bot.go
+++ pkg/app/bot.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/d-ashesss/noter-bot/pkg/model"
 	"gopkg.in/tucnak/telebot.v2"
 	"log"
 	"strings"
@@ -12,6 +13,14 @@ func initBotHandlers(b *telebot.Bot, a *App) {
 
 func (a App) botHandleTextMessage(m *telebot.Message) {
 	log.Printf("[bot] incoming message: %s: %s", getTelebotUserName(m.Sender), m.Text)
+	n := model.NewNote(m.Sender.ID, m.Text)
+	log.Printf("[bot] created new note %v", n)
+	_, err := a.bot.Send(m.Sender, n.Text)
+	if err != nil {
+		log.Printf("[bot] note response: %s", err)
+	} else {
+		_ = a.bot.Delete(m)
+	}
 }
 
 func getTelebotUserName(user *telebot.User) string {
