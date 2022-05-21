2ef9b1f75688024fbcb859eeae8bb3752c0f9a1e
Serhii Mashentsev <d.ashesss@gmail.com>
2022-05-21 23:24:27 +0300

    add my notes menu

--- pkg/app/bot.go
+++ pkg/app/bot.go
@@ -7,41 +7,31 @@ import (
 )
 
 const (
-	BotCommandStart = "/start"
+	BotCommandStart   = "/start"
+	BotCommandMyNotes = "/mynotes"
 
 	BotMessageWelcome = "*Welcome!*\n" +
 		"With this bot you will be able to save and manage your personal notes.\n" +
 		"To create a note simply send me a text message."
+
 	BotMessageNoteFailedToSave   = "😥 Failed to save this note."
 	BotMessageNoteFailedToDelete = "❗️ Failed to delete the note."
 	BotMessageNoteDeleted        = "✅ Note deleted."
+
+	BotMessageMyNotesMenu = "What would you like to do with your notes?"
 )
 
 func (a *App) RegisterBotHandlers() {
 	a.bot.Handle(BotCommandStart, a.botHandleCommandStart)
+	a.bot.Handle(BotCommandMyNotes, a.botHandleCommandMyNotes)
+
 	a.bot.Handle(telebot.OnText, a.botHandleMessageText)
 
 	noteOptionsMenu := NewBotMenuNoteOptions("")
-	a.bot.Handle(noteOptionsMenu.BtnDelete, a.botHandleCallbackNoteOptionsDelete)
-}
+	a.bot.Handle(&noteOptionsMenu.BtnDelete, a.botHandleCallbackNoteOptionsDelete)
 
-func (a *App) botHandleCallbackNoteOptionsDelete(cb *telebot.Callback) {
-	log.Printf("[bot] request to delete note %q", cb.Data)
-	n, err := a.noteModel.Get(a.botCtx, cb.Data)
-	if err == nil {
-		if err := a.noteModel.Delete(a.botCtx, n); err != nil {
-			log.Printf("[bot] failed to delete note %s: %s", n.ID, err)
-			_ = a.bot.Respond(cb, &telebot.CallbackResponse{Text: BotMessageNoteFailedToDelete, ShowAlert: true})
-			return
-		}
-		log.Printf("[bot] note %s was deleted", n.ID)
-	} else {
-		log.Printf("[bot] note %q was not found: %s", cb.Data, err)
-	}
-	_ = a.bot.Respond(cb, &telebot.CallbackResponse{Text: BotMessageNoteDeleted})
-	if err := a.bot.Delete(cb.Message); err != nil {
-		log.Printf("[bot] failed to delete message with note %s: %s", n.ID, err)
-	}
+	myNotesMenu := NewBotMenuMyNotes()
+	a.bot.Handle(&myNotesMenu.BtnShowAll, a.botHandleCallbackMyNotesShowAll)
 }
 
 func (a *App) botHandleCommandStart(m *telebot.Message) {
@@ -52,6 +42,21 @@ func (a *App) botHandleCommandStart(m *telebot.Message) {
 	); err != nil {
 		log.Printf("[bot] failed to welcome user: %s", err)
 	}
+}
+
+func (a *App) botHandleCommandMyNotes(m *telebot.Message) {
+	if _, err := a.bot.Send(
+		m.Sender,
+		BotMessageMyNotesMenu,
+		&telebot.SendOptions{ParseMode: telebot.ModeMarkdown},
+		NewBotMenuMyNotes().Menu,
+	); err != nil {
+		log.Printf("[bot] failed to show my notes menu: %s", err)
+	}
+}
+
+func (a App) botHandleCallbackMyNotesShowAll(cb *telebot.Callback) {
+	_ = a.bot.Respond(cb, &telebot.CallbackResponse{Text: "ok"})
 }
 
 func (a *App) botHandleMessageText(m *telebot.Message) {
@@ -77,5 +82,24 @@ func (a *App) botHandleMessageText(m *telebot.Message) {
 	}
 	if err := a.bot.Delete(m); err != nil {
 		log.Printf("[bot] failed to delete original for note %s: %s", n.ID, err)
+	}
+}
+
+func (a *App) botHandleCallbackNoteOptionsDelete(cb *telebot.Callback) {
+	log.Printf("[bot] request to delete note %q", cb.Data)
+	n, err := a.noteModel.Get(a.botCtx, cb.Data)
+	if err == nil {
+		if err := a.noteModel.Delete(a.botCtx, n); err != nil {
+			log.Printf("[bot] failed to delete note %s: %s", n.ID, err)
+			_ = a.bot.Respond(cb, &telebot.CallbackResponse{Text: BotMessageNoteFailedToDelete, ShowAlert: true})
+			return
+		}
+		log.Printf("[bot] note %s was deleted", n.ID)
+	} else {
+		log.Printf("[bot] note %q was not found: %s", cb.Data, err)
+	}
+	_ = a.bot.Respond(cb, &telebot.CallbackResponse{Text: BotMessageNoteDeleted})
+	if err := a.bot.Delete(cb.Message); err != nil {
+		log.Printf("[bot] failed to delete message with note %s: %s", n.ID, err)
 	}
 }
--- pkg/app/bot_menu.go
+++ pkg/app/bot_menu.go
@@ -11,7 +11,7 @@ const (
 type BotMenuNoteOptions struct {
 	Menu *telebot.ReplyMarkup
 
-	BtnDelete *telebot.Btn
+	BtnDelete telebot.Btn
 }
 
 // NewBotMenuNoteOptions initializes new BotMenuNoteOptions.
@@ -19,8 +19,42 @@ func NewBotMenuNoteOptions(noteID string) *BotMenuNoteOptions {
 	m := &BotMenuNoteOptions{
 		Menu: &telebot.ReplyMarkup{},
 	}
-	btnDelete := m.Menu.Data(BotMenuNoteOptionsBtnDeleteLabel, BotMenuNoteOptionsBtnDeleteID, noteID)
-	m.BtnDelete = &btnDelete
-	m.Menu.Inline(m.Menu.Row(btnDelete))
+	m.BtnDelete = m.Menu.Data(BotMenuNoteOptionsBtnDeleteLabel, BotMenuNoteOptionsBtnDeleteID, noteID)
+	m.Menu.Inline(
+		m.Menu.Row(m.BtnDelete),
+	)
+	return m
+}
+
+const (
+	BotMenuMyNotesBtnShowAllLabel   = "Show all"
+	BotMenuMyNotesBtnShowAllID      = "btnMenuMyNotesShowAll"
+	BotMenuMyNotesBtnShowFirstLabel = "Show first"
+	BotMenuMyNotesBtnShowFirstID    = "btnMenuMyNotesShowFirst"
+	BotMenuMyNotesBtnShowLastLabel  = "Show last"
+	BotMenuMyNotesBtnShowLastID     = "btnMenuMyNotesShowLast"
+)
+
+// BotMenuMyNotes represents My notes menu.
+type BotMenuMyNotes struct {
+	Menu *telebot.ReplyMarkup
+
+	BtnShowAll   telebot.Btn
+	BtnShowFirst telebot.Btn
+	BtnShowLast  telebot.Btn
+}
+
+// NewBotMenuMyNotes initializes new BotMenuMyNotes.
+func NewBotMenuMyNotes() *BotMenuMyNotes {
+	m := &BotMenuMyNotes{
+		Menu: &telebot.ReplyMarkup{},
+	}
+	m.BtnShowAll = m.Menu.Data(BotMenuMyNotesBtnShowAllLabel, BotMenuMyNotesBtnShowAllID)
+	m.BtnShowFirst = m.Menu.Data(BotMenuMyNotesBtnShowFirstLabel, BotMenuMyNotesBtnShowFirstID)
+	m.BtnShowLast = m.Menu.Data(BotMenuMyNotesBtnShowLastLabel, BotMenuMyNotesBtnShowLastID)
+	m.Menu.Inline(
+		m.Menu.Row(m.BtnShowAll),
+		m.Menu.Row(m.BtnShowFirst, m.BtnShowLast),
+	)
 	return m
 }
